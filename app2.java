840c2e2d209bb18473eb4236120df2db2229613d
Benjamin Philip Hays <67854569+ultradestroyer9123@users.noreply.github.com>
2025-05-12 15:06:49 -0700

    Delete app2.java

--- app2.java
+++
@@ -1,8 +0,0 @@
-public class app2 {
-  public static void main(String[] args) {
-    ChessEngine engine = new ChessEngine("white");
-    UserInterface uis = new UserInterface(engine);
-    
-  }
-
-}
